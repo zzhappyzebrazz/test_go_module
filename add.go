9eb11cda0af4afe7645ae183bbc27c7eda12b060
Lại Minh Hà <b8.bbl.lmh@gmail.com>
2025-02-04 21:57:31 +0700

    Adding go doc comment for versin v1.0.1

--- add.go
+++ add.go
@@ -1,5 +1,10 @@
+// Package testgomodule is a module written by zzhappyzebrazz under the context of the book Learning go by Jon Bodner
+// this is a test package for the exercises in the text book
 package testgomodule
 
+// Add takes two int parameters and return the int sum of the two
+// be sure to inclue a link to [math is fun]: https//www.mathisfun.com/numbers/addition.html in your Add functin godoc
+// [math is fun]: https//www.mathisfun.com/numbers/addition.html
 func Add(p1, p2 int) int {
 	return p1 + p2
 }
